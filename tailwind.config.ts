bb69d01da168cb3bac2bd491c26083533f306e0d
Your Name <shabbirzain314@gmail.com>
2025-04-11 18:46:24 +0500

    push

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,12 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
-    "./src/app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+   
+      "./src/pages/**/*.{ts,tsx}",
+      "./src/components/**/*.{ts,tsx}",
+      "./src/app/**/*.{ts,tsx}",
+      "./src/**/*.{ts,tsx}",
+    
   ],
   prefix: "",
   theme: {
